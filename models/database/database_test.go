f8b8c833695a174b5268ca2a66202b01bdd65ef4
Arthur Gregório <arthurgregorioleal@gmail.com>
2021-06-19 12:37:37 -0300

    Tests: add tests to database

--- models/database/database_test.go
+++ models/database/database_test.go
@@ -4,12 +4,36 @@ import (
 	testing "testing"
 )
 
+var database Database
+
 func TestDatabase(t *testing.T) {
-	var database Database
+	genDatabase()
+
+	domains := database.ListDomainsByName()
+	if len(domains) == 5 {
+		t.Logf("Success, expected size: %d and received %d", 5, len(domains))
+	} else {
+		t.Errorf("Failed, expected size: %d and received %d", 5, len(domains))
+	}
+
+}
+
+func TestDatabase2(t *testing.T) {
+	genDatabase()
+
+	domains := database.GetDomainByName("Test")
+	if len(domains) == 3 {
+		t.Logf("Success, expected size: %d and received %d", 3, len(domains))
+	} else {
+		t.Errorf("Failed, expected size: %d and received %d", 3, len(domains))
+	}
+}
+
+func genDatabase() {
 	database.Instantiate()
 	database.AddDomain("Test")
-	domains := database.ListDomains()
-	for _, domain := range domains {
-		t.Logf(domain)
-	}
+	database.AddDomain("Test3")
+	database.AddDomain("Test")
+	database.AddDomain("Test2")
+	database.AddDomain("Test")
 }
